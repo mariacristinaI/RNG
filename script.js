a85dc540ab6626bda34028ffa39bf7789e6c2939
Maria-Cristina Iosub <75473536+mariacristinaI@users.noreply.github.com>
2023-01-25 18:48:28 +0200

    Update

--- script.js
+++ script.js
@@ -1,22 +1,17 @@
 function randomNumberGenerator() {
-    let min = parseInt(document.getElementById('min').value);
-    let max = parseInt(document.getElementById('max').value);
-    let finalnumber = Math.floor(Math.random() * (max - min + 1) + min); 
-    document.getElementById('result').value = finalnumber;
-}
-
-let animateButton = function(e) {
-    e.preventDefault;
-    e.target.classList.remove('animate');
-    e.target.classList.add('animate');
-    setTimeout(function(){
-      e.target.classList.remove('animate');
-    },700);
-  };
-  let confetti = document.getElementsByClassName("confetti-button");
-  for (let i = 0; i < confetti.length; i++) {
-    confetti[i].addEventListener('click', animateButton, false);
+  let min = parseInt(document.getElementById('min').value);
+  let max = parseInt(document.getElementById('max').value);
+  if (isNaN(min) || isNaN(max)) {
+      alert("Error: Min and max values must be numbers.");
+      return;
   }
+  if (max <= min) {
+      alert("Error: Max value must be greater than min value.");
+      return;
+  }
+  let finalnumber = Math.floor(Math.random() * (max - min) + min);
+  document.getElementById('result').value = finalnumber;
+}
 
 const today = new Date();
 let currentYear = today.getFullYear();
